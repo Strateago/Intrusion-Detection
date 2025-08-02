089f83c978e967907ca4f883579d7fcafaaa131b
tjgc <tjgc@cin.ufpe.br>
2025-08-02 19:47:47 -0300

    Add a script to preprocess SOMEIP

---
+++ preprocess_someip.py
@@ -0,0 +1,29 @@
+import argparse
+import pandas as pd
+import numpy as np
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description="Preprocess the SOMEIP dataset")
+    parser.add_argument("--path", type=str, required=True, help="Base path where the dataset is located.")
+    args = parser.parse_args()
+    path = args.path
+
+    os.makedirs(f'{path}/Preprocessed', exist_ok=True)
+    print("Start aggregating X")
+    for file in os.listdir(path):
+        if not os.path.isfile(f'{path}/{file}'):
+            continue
+        data = pd.read_pickle(f'{path}/{file}')
+        if 'X' in file:
+            data = np.sum(data, axis=1)
+        else:
+            print(np.unique(data))
+        name = file.split('.')[0]
+        print(name, ' ', data.shape)
+        np.save(f'{path}/Preprocessed/{name}', data)
+
+    print('Done')
+
+if __name__ == "__main__":
+    main()
