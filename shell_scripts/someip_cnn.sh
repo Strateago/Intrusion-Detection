ffd166f4d53cd32ca31e722312d6800aa4368818
Thiago Costa <104111765+Strateago@users.noreply.github.com>
2025-08-15 16:24:58 -0300

    Update path someip_cnn.sh

--- shell_scripts/someip_cnn.sh
+++ shell_scripts/someip_cnn.sh
@@ -12,13 +12,13 @@
 module load Python/3.10
 
 # Ambient activate
-source $HOME/automotive-ids-evaluation-framework/venv/bin/activate
+source $HOME/Intrusion-Detection/venv/bin/activate
 
 # Model train
-python3 $HOME/automotive-ids-evaluation-framework/execute_model_train_validation.py --model_train_valid_config config_jsons/SOMEIP/model_train/CNN_train_val_config.json
+python3 $HOME/Intrusion-Detection/scripts/execute_model_train_validation.py --model_train_valid_config config_jsons/SOMEIP/model_train/CNN_train_val_config.json
 
 # Model test
-python3 $HOME/automotive-ids-evaluation-framework/execute_model_test.py --model_test_config config_jsons/SOMEIP/model_test/CNN_test_config.json
+python3 $HOME/Intrusion-Detection/scripts/execute_model_test.py --model_test_config config_jsons/SOMEIP/model_test/CNN_test_config.json
 
 # Metrics extraction
-python3 $HOME/automotive-ids-evaluation-framework/plot_metrics.py --path output/SOMEIP/CNN
+python3 $HOME/Intrusion-Detection/scripts/plot_metrics.py --path output/SOMEIP/CNN
